cef0cbeda75e061c3e85d02da08b95c506810b82
Xaya <contact.xaya@gmail.com>
2025-03-29 16:55:19 +0100

    Initial commit

---
+++ src/main/kotlin/fr/xania/legendsTwilight/LegendsTwilight.kt
@@ -0,0 +1,14 @@
+package fr.xania.legendsTwilight
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LegendsTwilight : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
