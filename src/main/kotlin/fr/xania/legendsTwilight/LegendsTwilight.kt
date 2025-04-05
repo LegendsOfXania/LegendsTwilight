bfd786dd686f64957d867a1895c5bd4a45edb7d3
LegendsOfXania <contact.xaya@gmail.com>
2025-04-05 23:54:07 +0200

    Add Config Manager

--- src/main/kotlin/fr/xania/legendsTwilight/LegendsTwilight.kt
+++ src/main/kotlin/fr/xania/legendsTwilight/LegendsTwilight.kt
@@ -8,7 +8,6 @@ class LegendsTwilight : JavaPlugin() {
 
         enableLogger()
 
-        saveDefaultConfig()
     }
 
     override fun onDisable() {
---
+++ src/main/kotlin/fr/xania/legendsTwilight/utils/ConfigManager.kt
@@ -0,0 +1,51 @@
+package fr.xania.legendsTwilight.utils
+
+import com.google.gson.Gson
+import org.bukkit.Bukkit
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+data class Config(val enabled_worlds: List<String>)
+
+object ConfigManager {
+
+    private val gson = Gson()
+
+    private fun getConfigFile(): File {
+        return File(Bukkit.getPluginManager().getPlugin("LegendsTwilight")!!.dataFolder, "config.json")
+    }
+
+    fun loadConfig(): Config {
+        val file = getConfigFile()
+
+        if (!file.exists()) {
+            val defaultConfig = Config(enabled_worlds = listOf("world", "world_the_end"))
+            saveConfig(defaultConfig)
+            return defaultConfig
+        }
+
+        val json = String(Files.readAllBytes(Paths.get(file.toURI())))
+        return gson.fromJson(json, Config::class.java)
+    }
+
+    fun saveConfig(config: Config) {
+        val json = gson.toJson(config)
+        Files.write(getConfigFile().toPath(), json.toByteArray())
+    }
+    fun addWorld(world: String) {
+        val config = loadConfig()
+        if (!config.enabled_worlds.contains(world)) {
+            val updatedConfig = config.copy(enabled_worlds = config.enabled_worlds + world)
+            saveConfig(updatedConfig)
+        }
+    }
+
+    fun removeWorld(world: String) {
+        val config = loadConfig()
+        if (config.enabled_worlds.contains(world)) {
+            val updatedConfig = config.copy(enabled_worlds = config.enabled_worlds - world)
+            saveConfig(updatedConfig)
+        }
+    }
+}
